64265d75cd2ee9af37a624359023f8ef97bbd9e2
LimDae94 <piay801@naver.com>
2024-04-19 19:04:52 +0900

    동적페이지 이동
    동적페이지 이동

--- mall/src/router/todoRouter.js
+++ mall/src/router/todoRouter.js
@@ -1,11 +1,11 @@
 import {lazy, Suspense} from "react";
 import {Navigate} from "react-router-dom";
-import AddPage from "../pages/todo/AddPage";
 
 const Loading = <div>Loading...</div>
 const TodoList = lazy(() => import("../pages/todo/ListPage"))
 const TodoRead = lazy(() => import("../pages/todo/ReadPage"))
 const TodoAdd = lazy(() => import("../pages/todo/AddPage"))
+const TodoModify = lazy(() => import("../pages/todo/ModifyPage"))
 
 const todoRouter = () => {
     return [
@@ -23,7 +23,11 @@ const todoRouter = () => {
         },
         {
             path: 'add',
-            element: <Suspense fallback={Loading}><AddPage/></Suspense>
+            element: <Suspense fallback={Loading}><TodoAdd/></Suspense>
+        },
+        {
+            path: 'modify/:tno',
+            element: <Suspense fallback={Loading}><TodoModify/></Suspense>
         }
     ]
 }
