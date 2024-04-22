687618770301fa10e29d31fca6b46ad9152efc16
LimDae94 <piay801@naver.com>
2024-04-22 13:31:46 +0900

    Querydsl 설정하기
    QuerydslRepositorySupport 를 상속받는 TodoSearch 검색 인터페이스와 단일 구현 객체를 의미하는 Impl 클래스가 핵심.

--- mallapi/src/test/java/org/zerock/mallapi/repository/TodoRepositoryTest.java
+++ mallapi/src/test/java/org/zerock/mallapi/repository/TodoRepositoryTest.java
@@ -1,7 +1,102 @@
 package org.zerock.mallapi.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.zerock.mallapi.domain.Todo;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+@SpringBootTest
+@Log4j2
 class TodoRepositoryTest {
 
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Test
+    public void test1() {
+        Assertions.assertNotNull(todoRepository);
+        log.info(todoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsert() {
+
+        for (int i = 0; i < 100; i++) {
+
+
+            Todo todo = Todo.builder()
+                    .title("Title")
+                    .content("Content...")
+                    .dueDate(LocalDate.of(2024, 4, 30))
+                    .build();
+
+            Todo save = todoRepository.save(todo);
+
+            log.info(save);
+
+        }
+    }
+
+    @Test
+    public void testRead() {
+        Long tno = 1L;
+
+        Optional<Todo> result = todoRepository.findById(tno);
+
+        Todo todo = result.orElseThrow();
+
+        log.info(todo);
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+        // 먼저 로딩하고 엔티티 객체를 변경 /setter
+        Long tno = 1L;
+
+        Optional<Todo> result = todoRepository.findById(tno);
+
+        Todo todo = result.orElseThrow();
+
+        todo.changeTitle("Update Title");
+        todo.changeContent("updated content");
+        todo.changeComplete(true);
+
+        todoRepository.save(todo);
+
+    }
+
+    /**
+     * 10건 이상 조회가 안된다면, count 쿼리를 만들지 않습니다. 1 건만 저장된 경우에는 limit 쿼리만 수행됩니다.
+     */
+    @Test
+    public void testPaging() {
+
+        // 페이지 시작 번호: 0
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("tno").descending());
+
+        Page<Todo> result = todoRepository.findAll(pageable);
+
+        log.info(result.getTotalElements());
+
+        log.info(result.getContent());
+
+    }
+
+    @Test
+    public void testSearch1() {
+
+        todoRepository.search1();
+
+    }
 }
